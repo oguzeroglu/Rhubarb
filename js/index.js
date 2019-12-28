29c25ef661f84e5414e4d56c2b48a9f661bac39b
oguz <eroglu.oguz93@gmail.com>
2019-12-29 00:52:51 +0100

    fancy ascii art

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 import ProtocolParser from "./ProtocolParser";
 
 var Rhubarb = function(){
+  var banner = "%c";
+  banner += "______ _           _                _     " + "\n";
+  banner += "| ___ \\ |         | |              | |    " + "\n";
+  banner += "| |_/ / |__  _   _| |__   __ _ _ __| |__  " + "\n";
+  banner += "|    /| '_ \\| | | | '_ \\ / _` | '__| '_ \\ " + "\n";
+  banner += "| |\\ \\| | | | |_| | |_) | (_| | |  | |_) |" + "\n";
+  banner += "\\_| \\_|_| |_|\\__,_|_.__/ \\__,_|_|  |_.__/ " + "\n";
+  banner += "                                          " + "\n";
+  banner += "                           by Oguz Eroglu " + "\n";
+  banner += "                                          " + "\n";
 
+  console.log(banner, "background: black; color: lime");
 }
 
 Rhubarb.prototype.init = function(protocolDefinitionPath){
